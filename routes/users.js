e8b8c46b28c94e54a60575b4a7c0a7cfb31e0fbc
NatC02 <NatanCeballos02@gmail.com>
2021-05-25 08:42:32 -0400

    Finished setting up basic handlebars with bootstrap included startup css and js as well

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Get Register
-router.get('/register', function(req, res) {
-    res.render('register');
+router.get("/register", function (req, res) {
+  res.render("register");
 });
 
 // Login
-router.get('/login', function(req, res) {
-    res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
 module.exports = router;
